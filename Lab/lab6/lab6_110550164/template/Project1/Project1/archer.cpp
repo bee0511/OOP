01dd0f4ab0e258355303dfd7d9afc4aee4aabde3
bee.yu <97232591+bee0511@users.noreply.github.com>
2022-07-10 10:45:52 +0800

    packed to zip files and add Labs

--- Lab/lab6/lab6_110550164/template/Project1/Project1/archer.cpp
+++
@@ -1,8 +0,0 @@
-// TODO : refer to spec
-#include "archer.h"
-Archer::Archer(string roleID, int roleLevel, int roleMoney, int roleHp) : Role(roleID, roleLevel, roleMoney, roleHp) {
-
-}
-void Archer::ShowRole() {
-	cout << "Role " << getID() << " is Archer" << endl;
-}
